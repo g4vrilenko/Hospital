4cced62e584738f6ba00fe1f162b200cc1d5882d
Alex Sikilinda <alex.sikilinda@gmail.com>
2016-10-23 09:59:02 +0300

    Initial commit

---
+++ Hospital/Models/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 1 - Arrived
+        /// 2 - Sick
+        /// 3 - Healthy
+        /// 
+        /// TODO: change it to enum
+        /// </summary>
+        public int Status { get; set; }
+
+        public DateTime DayOfBirth { get; set; }
+
+        /// <summary>
+        /// Идентификационный код.
+        /// </summary>
+        public string TaxCode { get; set; }
+
+        public virtual ICollection<Doctor> Doctors { get; set; }
+    }
+}
